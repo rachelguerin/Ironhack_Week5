ef5733c83ab37128aec073e86eb8d5f75552eccc
Rachel Guerin <rachel.guerin23@gmail.com>
2016-02-29 16:10:57 +0100

    pizza builder calc cost

--- pizza.js
+++ pizza.js
@@ -13,28 +13,57 @@ $('.sauce-cost').hide();
 $('.crust-cost').hide();
 $('.price strong').text("$"+total);
 
-adjust_price = function (cost,ingredient) {
-	if ($(ingredient).hasClass('active')) {
-		total -= cost;
-	} else {
-		total += cost;
-	}
+function adjust_price(cost,ingredient) {
+		if ($(ingredient).hasClass('active')){
+			total -= cost;
+		} else {
+			total += cost;
+		};
 }
 
 
-$('.btn-pepperonni').on('click',function () {$('.pep').toggle(); 
+$('.btn-pepperonni').on('click',function () { 
+		$('.pep').toggle(); 
 		$('.btn-pepperonni').toggleClass('active'); 
-		$('.pep-cost').toggle(); 
-		adjust_price(1,'.btn_pepperonni');
+		$('.pep-cost').toggle(); 		
+		adjust_price(1,'.btn-pepperonni');
+		$('.price strong').text("$"+total);
+
+	});
+
+
+
+$('.btn-mushrooms').on('click',function () {
+		$('.mushroom').toggle(); 
+		$('.btn-mushrooms').toggleClass('active'); 
+		$('.mush-cost').toggle(); 
+		adjust_price(1,'.btn-mushrooms');
 		$('.price strong').text("$"+total);
 	});
 
-$('.btn-mushrooms').on('click',function () {$('.mushroom').toggle(); $('.btn-mushrooms').toggleClass('active'); $('.mush-cost').toggle() });
+$('.btn-green-peppers').on('click',function () {
+		$('.green-pepper').toggle(); 
+		$('.btn-green-peppers').toggleClass('active'); 
+		$('.green-pep-cost').toggle();
+		adjust_price(1,'.btn-green-peppers');
+		$('.price strong').text("$"+total);
 
-$('.btn-green-peppers').on('click',function () {$('.green-pepper').toggle(); $('.btn-green-peppers').toggleClass('active'); $('.green-pep-cost').toggle() });
+	});
 
-$('.btn-sauce').on('click',function () {$('.sauce').toggle(); $('.btn-sauce').toggleClass('active'); $('.sauce-cost').toggle() });
+$('.btn-sauce').on('click',function () {
+		$('.sauce').toggle(); 
+		$('.btn-sauce').toggleClass('active'); 
+		$('.sauce-cost').toggle();
+		adjust_price(3,'.btn-sauce');
+		$('.price strong').text("$"+total); 
+	});
 
-$('.btn-crust').on('click',function () {$('.crust-gluten-free').toggle(); $('.btn-crust').toggleClass('active'); $('.crust-cost').toggle() });
+$('.btn-crust').on('click',function () {
+		$('.crust-gluten-free').toggle(); 
+		$('.btn-crust').toggleClass('active'); 
+		$('.crust-cost').toggle();
+		adjust_price(5,'.btn-crust');
+		$('.price strong').text("$"+total); 
+	});
 
 
